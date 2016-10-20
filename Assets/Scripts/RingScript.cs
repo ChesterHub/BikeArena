2f507a21752e3663312ecf725d335e167f0f9d38
Maquiz <maximilian.bolling@gmail.com>
2016-10-19 21:03:14 -0700

    Rings and Sound
    Added Rings which explode with a sound and confetti
    Bug
    -Confetti does not stop
    -Sound can be triggered again

---
+++ Assets/Scripts/RingScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RingScript : MonoBehaviour {
+    public GameObject confetti;
+    public MeshRenderer mr;
+    public AudioSource collectSound;
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name.ToString());
+        if (other.name.ToString() == "GenericMotorBike") {
+            //this.gameObject.SetActive(false);
+            mr.enabled = false;
+            confetti.SetActive(true);
+            collectSound.Play();
+
+        }
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
